bcf07ccd6584b322d261dcac9992b5900aa08547
Matheus CR <102773415+MatheusCRNETBIOS@users.noreply.github.com>
2022-04-13 19:13:16 -0300

    Add files via upload

---
+++ Qual seu nome.rb
@@ -0,0 +1,8 @@
+puts "Qual é o seu nome?"
+nome = gets.chomp
+puts " Qual é a sua idade?"
+idade = gets.chomp.to_i
+
+
+puts "Olá o seu nome #{nome} é horrível "
+puts "você #{nome} tem #{idade} anos, ?"
---
+++ Soma de números.rb
@@ -0,0 +1,9 @@
+puts " Insira um número"
+x = gets.chomp.to_i
+
+puts "Insira outro número "
+y = gets.chomp.to_i
+
+soma =x + y
+subt = x - y
+mult = x - y
